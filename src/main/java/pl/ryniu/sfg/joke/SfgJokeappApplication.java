48a64c2dc6a4ef726d51c025eee523350620b0dc
Ryniu <marcin3001@10g.pl>
2021-02-04 22:37:27 +0100

    Initial commit

---
+++ src/main/java/pl/ryniu/sfg/joke/SfgJokeappApplication.java
@@ -0,0 +1,13 @@
+package pl.ryniu.sfg.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ryniu/sfg/joke/SfgJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ryniu.sfg.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
