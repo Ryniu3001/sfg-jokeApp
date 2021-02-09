4101aebcf221b0e2082ba7f5af7e9621a40a17b5
Ryniu <marcin3001@10g.pl>
2021-02-09 21:34:09 +0100

    test xml based configuration

--- src/main/java/pl/ryniu/sfg/joke/config/ChuckConfiguration.java
+++ src/main/java/pl/ryniu/sfg/joke/config/ChuckConfiguration.java
@@ -1,15 +1,12 @@
 package pl.ryniu.sfg.joke.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-	@Bean
-	public ChuckNorrisQuotes chuckNorrisQuotes() {
+//	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes2() {
 		return new ChuckNorrisQuotes();
 	}
 }
